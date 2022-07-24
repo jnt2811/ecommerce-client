697abcb96a78830ea878a3f3a65773f31b19158d
ThanhNt <admater99@gmail.com>
2022-07-24 13:32:49 +0700

    before pull

--- src/constants/keys.js
+++ src/constants/keys.js
@@ -1,7 +1,7 @@
 export const keys = {
   ACCESS_TOKEN: "access_token",
   USER_INFO: "user_info",
-  SERVER_URI: "https://0004-123-16-146-8.ap.ngrok.io",
+  SERVER_URI: "http://localhost:8082",
   PAYPAL_SANDBOX_CLIENT_ID:
     "AcYsWy99ZCtgwVYvL9I6_hYbj7Wa6U8haf-cSC0F-DbtWfIPKFaLRFiUaqeH1QjojZwgC0DvmSiQimxS",
 };
