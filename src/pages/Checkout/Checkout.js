7229da5d7c618bfd150fec65519ec79ba8fc59e6
jnt2811 <tranquangnguyen2811@gmail.com>
2022-07-25 00:37:49 +0700

    .

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,13 +1,15 @@
-import { TagOutlined } from "@ant-design/icons";
-import { Button, Col, Divider, Radio, Row, Space, Form, Input } from "antd";
+import { CloseOutlined, EditOutlined, TagOutlined } from "@ant-design/icons";
+import { Button, Col, Divider, Radio, Row, Space, Form, Input, Image } from "antd";
 import style from "./checkout.module.scss";
-import {useLazyQuery, useMutation} from "@apollo/client";
-import {useEffect, useState} from "react";
+import { useLazyQuery, useMutation } from "@apollo/client";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import {GET_USER_AND_CART} from "../../queries";
-import {PayPalButtons, PayPalScriptProvider} from "@paypal/react-paypal-js";
-import {keys} from "../../constants";
-import {ADD_NEW_ORDER} from "../../queries/order.gql";
+import { GET_USER_AND_CART } from "../../queries";
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
+import { keys, paths } from "../../constants";
+import { formatNumberToPrice } from "../../helpers";
+import { ADD_NEW_ORDER } from "../../queries/order.gql";
+import { useHistory } from "react-router-dom";
 
 const initialOptions = {
   "client-id": keys.PAYPAL_SANDBOX_CLIENT_ID,
@@ -18,74 +20,105 @@ const initialOptions = {
 
 export const Checkout = () => {
   const currentUser = useSelector((state) => state.auth.user);
-  const [visible, setVisible] = useState(false);
   const [deliveryInformation, setDeliveryInformation] = useState({});
   const [getCart, { loading: list_loading, error: list_error, data: list_data }] = useLazyQuery(
-      GET_USER_AND_CART,
-      {
-        variables: { userId: currentUser?.ID },
-        fetchPolicy: "no-cache",
-        onCompleted: data => {
-          setDeliveryInformation({
-            NAME: `${data.getUsers[0].FIRST_NAME} ${data.getUsers[0].LAST_NAME}`,
-            PHONE_NUMBER: data.getUsers[0].PHONE_NUMBER,
-            ADDRESS: data.getUsers[0].ADDRESS
-          })
-        }
-      });
-
-  const [addNewOrder, { loading: list_order_loading, error: list_order_error, data: list_order_data}] = useMutation(
-      ADD_NEW_ORDER
+    GET_USER_AND_CART,
+    {
+      variables: { userId: currentUser?.ID },
+      fetchPolicy: "no-cache",
+      onCompleted: (data) => {
+        setDeliveryInformation({
+          NAME: `${data.getUsers[0].FIRST_NAME} ${data.getUsers[0].LAST_NAME}`,
+          PHONE_NUMBER: data.getUsers[0].PHONE_NUMBER,
+          ADDRESS: data.getUsers[0].ADDRESS,
+        });
+      },
+    }
   );
+  const [paymentMethod, setPaymentMethod] = useState("cod");
+  const [visibleDeliveryForm, setVisibleDeliveryForm] = useState(false);
+  const history = useHistory();
+  const [orderId, setOrderId] = useState("");
+
+  const [
+    addNewOrder,
+    { loading: list_order_loading, error: list_order_error, data: list_order_data },
+  ] = useMutation(ADD_NEW_ORDER, {
+    onCompleted: () => history.push(paths.order + `/${orderId}`),
+  });
 
   useEffect(() => {
-    getCart()
+    getCart();
   }, [currentUser?.ID, getCart]);
 
   console.log("get cart", list_data, list_loading, list_error);
   console.log("add new order", list_order_loading, list_order_error, list_order_data);
 
-  const productTotalPrice = list_data?.getCart.reduce((p,c) => p.COUNT_PRODUCT*p.PRICE + c.COUNT_PRODUCT*c.PRICE)
-  const deliveryTotalPrice = list_data?.getCart.reduce((p,c) => p.DELIVERY_PRICE+c.DELIVERY_PRICE)
-  const discountTotalPrice = 100000
-  const totalPrice = productTotalPrice + deliveryTotalPrice - discountTotalPrice
+  const productTotalPrice = list_data?.getCart.reduce(
+    (sum, item) => sum + item.COUNT_PRODUCT * item.PRICE,
+    0
+  );
+  const deliveryTotalPrice = list_data?.getCart.reduce((sum, item) => sum + item.DELIVERY_PRICE, 0);
+  const discountTotalPrice = 100000;
+  const totalPrice = productTotalPrice + deliveryTotalPrice - discountTotalPrice;
 
   const onSuccessPaypalPayment = (details) => {
-    let orderID = details.id ? details.id : "O-" + (new Date()).getTime()
-    handleAddNewOrder(orderID)
-  }
+    let orderID = details.id ? details.id : "O-" + new Date().getTime();
+    handleAddNewOrder(orderID);
+  };
 
-  const onCreateOrder = (e) =>{
-    let orderID = "O-" + (new Date()).getTime()
-    handleAddNewOrder(orderID)
-  }
+  const onCreateOrder = (e) => {
+    let orderID = "O-" + new Date().getTime();
+    handleAddNewOrder(orderID);
+  };
 
-  const handleAddNewOrder = (orderID) =>{
+  const handleAddNewOrder = (orderID) => {
     let orderData = {
       ORDER_ID: orderID,
       USER_ID: currentUser?.ID,
       ...deliveryInformation,
       DELIVERY_METHOD: "COD",
       TOTAL_PRICE: totalPrice,
-      PRODUCTS: list_data.getCart.map(c => {
+      PRODUCTS: list_data.getCart.map((c) => {
         return {
           PRODUCT_ID: c.PRODUCT_ID,
-          QUANTITY: c.COUNT_PRODUCT
-        }
-      })
-    }
-    console.log(orderData)
-    addNewOrder({variables: {order: orderData}})
-  }
+          QUANTITY: c.COUNT_PRODUCT,
+        };
+      }),
+    };
+
+    console.log(orderData);
+
+    setOrderId(orderID);
+    addNewOrder({ variables: { order: orderData } });
+
+    // setTimeout(() => {
+    //
+    // }, 500);
+  };
 
   const onSuccessSetDeliveryInformation = (values) => {
-    setDeliveryInformation(values)
-  }
+    setDeliveryInformation(values);
+  };
 
   const onFailSetDeliveryInformation = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  }
+    console.log("Failed:", errorInfo);
+  };
 
+  const handleChangePaymentMethod = (e) => {
+    const { value } = e.target;
+    setPaymentMethod(value);
+  };
+
+  const renderProductImage = (item) => {
+    let src = "";
+    if (!!item?.GALLERY && JSON.parse(item?.GALLERY).length > 0) {
+      src = keys.SERVER_URI + JSON.stringify(item.GALLERY)[0];
+    }
+    return <Image preview={false} src={src} width={80} height={80} />;
+  };
+
+  console.log("aaaaaaabb", totalPrice);
 
   return (
     <div>
@@ -94,102 +127,140 @@ export const Checkout = () => {
           <div className={style["block"]}>
             <h3>Đơn hàng</h3>
 
-            {list_data?.getCart.map(item => (
-                <div className={style["wrapper"]} key={item.ID}>
-                  <Row justify="space-between">
-                    <Col>
-                      <div>{item.PRODUCT_NAME}</div>
-                      <div>{item.COUNT_PRODUCT}</div>
-                    </Col>
-                    <Col>{item.PRICE}</Col>
-                  </Row>
-                  <Divider />
-                  <Row justify="space-between">
-                    <Col>Vận chuyển</Col>
-                    <Col>{item.DELIVERY_PRICE}</Col>
-                  </Row>
-                </div>
+            {list_data?.getCart.map((item) => (
+              <div className={style["wrapper"]} key={item.ID}>
+                <Row justify="space-between">
+                  <Col>
+                    <Space size={20}>
+                      {renderProductImage(item)}
+
+                      <div>
+                        <div>{item.PRODUCT_NAME}</div>
+                        <div>{item.COUNT_PRODUCT}</div>
+                      </div>
+                    </Space>
+                  </Col>
+                  <Col>{formatNumberToPrice(item.PRICE)}</Col>
+                </Row>
+                <Divider />
+                <Row justify="space-between">
+                  <Col>Vận chuyển</Col>
+                  <Col>{formatNumberToPrice(item.DELIVERY_PRICE)}</Col>
+                </Row>
+              </div>
             ))}
           </div>
 
           <div className={style["block"]}>
             <h3>Chọn hình thức thanh toán</h3>
 
-            <Radio.Group>
+            <Radio.Group value={paymentMethod} onChange={handleChangePaymentMethod}>
               <Space direction="vertical">
-                <Radio value="cod" onClick={()=>{setVisible(true)}}>Thanh toán tiền mặt khi nhận hàng (COD)</Radio>
-                <Radio value="paypal" onClick={()=>{setVisible(false)}}>Thanh toán bằng Paypal</Radio>
+                <Radio value="cod">Thanh toán tiền mặt khi nhận hàng (COD)</Radio>
+                <Radio value="paypal">Thanh toán bằng Paypal</Radio>
               </Space>
-              <PayPalScriptProvider deferLoading={totalPrice ? true : false} options={initialOptions}>
-                <PayPalButtons
+
+              <div
+                style={{
+                  display: totalPrice && paymentMethod === "paypal" ? "block" : "none",
+                  marginTop: 20,
+                }}
+              >
+                <PayPalScriptProvider
+                  deferLoading={totalPrice ? true : false}
+                  options={initialOptions}
+                >
+                  <PayPalButtons
                     createOrder={(data, actions) => {
-                      return actions.order.create({
-                        description: "DISDIS",
-                        purchase_units: [
-                          {
-                            amount: {
-                              value: (Number(totalPrice)*0.000043).toFixed(2).toString(),
+                      return actions.order
+                        .create({
+                          description: "DISDIS",
+                          purchase_units: [
+                            {
+                              amount: {
+                                value: (Number(totalPrice) * 0.000043).toFixed(2).toString(),
+                              },
                             },
+                          ],
+                          application_context: {
+                            shipping_preference: "NO_SHIPPING",
                           },
-                        ],
-                        application_context: {
-                          shipping_preference: "NO_SHIPPING"
-                        }
-                      }).then((orderId) => {
-                        // Your code here after create the order
-                        return orderId;
-                      });
+                        })
+                        .then((orderId) => {
+                          // Your code here after create the order
+                          return orderId;
+                        });
                     }}
                     onApprove={(data, actions) => {
                       return actions.order.capture().then((details) => {
-                        onSuccessPaypalPayment(details)
+                        onSuccessPaypalPayment(details);
                         alert(`Thanh toán thành công, đang chuyển hướng bạn về trang đơn hàng`);
                       });
                     }}
-                />
-              </PayPalScriptProvider>
+                  />
+                </PayPalScriptProvider>
+              </div>
             </Radio.Group>
           </div>
         </Col>
 
         <Col flex="350px">
           <div className={style["block"]}>
-            <h3>Giao tới</h3>
+            <Row align="middle" justify="space-between" style={{ marginBottom: 10 }}>
+              <h3 style={{ marginBottom: 0 }}>Giao tới</h3>
+              {!visibleDeliveryForm && (
+                <Button
+                  icon={<EditOutlined />}
+                  type="link"
+                  onClick={() => setVisibleDeliveryForm(true)}
+                ></Button>
+              )}
+            </Row>
+
             <Space>
               <b>{deliveryInformation?.NAME}</b>
               <b>{deliveryInformation?.PHONE_NUMBER}</b>
             </Space>
             <div style={{ marginTop: 5 }}>{deliveryInformation?.ADDRESS}</div>
           </div>
+
           <Form
-              className={style["block"]}
-              name="deliveryInformation"
-              labelCol={{ span: 24 }}
-              wrapperCol={{ span: 24 }}
-              onFinish={onSuccessSetDeliveryInformation}
-              onFinishFailed={onFailSetDeliveryInformation}
+            className={style["block"]}
+            name="deliveryInformation"
+            layout="vertical"
+            onFinish={onSuccessSetDeliveryInformation}
+            onFinishFailed={onFailSetDeliveryInformation}
+            style={{ display: visibleDeliveryForm ? "block" : "none" }}
           >
-            <h3>Giao tới</h3>
+            <Row align="middle" justify="space-between" style={{ marginBottom: 10 }}>
+              <h3 style={{ marginBottom: 0 }}>Giao tới</h3>
+              <Button
+                icon={<CloseOutlined />}
+                type="link"
+                onClick={() => setVisibleDeliveryForm(false)}
+              ></Button>
+            </Row>
+
             <Form.Item
-                label="Tên người nhận"
-                name="NAME"
-                rules={[{ required: true, message: 'Vui lòng nhập tên người nhận!' }]}
+              label="Tên người nhận"
+              name="NAME"
+              rules={[{ required: true, message: "Vui lòng nhập tên người nhận!" }]}
             >
               <Input />
             </Form.Item>
 
             <Form.Item
-                label="Số điện thoại"
-                name="PHONE_NUMBER"
-                rules={[{ required: true, message: 'Vui lòng nhập số điện thoại!' }]}
+              label="Số điện thoại"
+              name="PHONE_NUMBER"
+              rules={[{ required: true, message: "Vui lòng nhập số điện thoại!" }]}
             >
               <Input />
             </Form.Item>
 
             <Form.Item
-                label="Địa chỉ giao hàng"
-                name="ADDRESS"
-                rules={[{ required: true, message: 'Vui lòng nhập địa chỉ giao hàng!' }]}
+              label="Địa chỉ giao hàng"
+              name="ADDRESS"
+              rules={[{ required: true, message: "Vui lòng nhập địa chỉ giao hàng!" }]}
             >
               <Input />
             </Form.Item>
@@ -214,33 +285,36 @@ export const Checkout = () => {
 
             <Row justify="space-between">
               <div>Tạm tính</div>
-              <div>{productTotalPrice}</div>
+              <div>{formatNumberToPrice(productTotalPrice)}đ</div>
             </Row>
 
             <Row justify="space-between">
               <div>Phí vận chuyển</div>
-              <div>{deliveryTotalPrice}</div>
+              <div>{formatNumberToPrice(deliveryTotalPrice)}đ</div>
             </Row>
 
             <Row justify="space-between">
               <div>Giảm giá</div>
-              <div>{discountTotalPrice}</div>
+              <div>{formatNumberToPrice(discountTotalPrice)}đ</div>
             </Row>
 
             <Divider className={style["divider"]} />
 
             <Row justify="space-between">
               <div>Tổng tiền</div>
-              <div>{totalPrice? totalPrice : 0}</div>
+              <div>{totalPrice ? formatNumberToPrice(totalPrice) : 0}đ</div>
             </Row>
 
-            <br />
-
-            {
-              visible? <Button type="primary" block onClick={(e) => onCreateOrder(e)}>
+            {paymentMethod === "cod" && (
+              <Button
+                type="primary"
+                block
+                onClick={(e) => onCreateOrder(e)}
+                style={{ marginTop: 20 }}
+              >
                 Đặt hàng
-              </Button> : <div></div>
-            }
+              </Button>
+            )}
           </div>
         </Col>
       </Row>
